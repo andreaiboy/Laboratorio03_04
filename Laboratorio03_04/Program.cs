8e453c9da8c05e3a98963e458d9e5b2eebed5600
Andrea <andreagiboy@gmail.com>
2023-03-11 11:47:14 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio03_04/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("a. Sumatoria");
+Console.WriteLine("b. tablas de multiplicar");
+
+char opcion = Convert.ToChar(Console.ReadLine());
+
+switch (opcion)
+{
+    case 'a':
+        Console.WriteLine("Ingrese un número; ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int resultado = 0;
+
+        do
+        {
+            resultado = resultado + n;
+            n--;
+        } while (n > 0);
+
+        Console.WriteLine("Sumatoria" + resultado);
+        break;
+    case 'b':
+        for (int i =1; i < 11; i++)
+        {
+            Console.WriteLine(i + ": ");
+            for (int j = 1; j < 11; i++)
+            {
+                Console.WriteLine(i * j + "\t");
+            }
+            Console.Write("\n");
+        }
+        break;
+}
